06deb3f6beba2b32ed0d67a3191acfcc47e00a85
Nabin Singh <nabin@skilluplabs.com.au>
2024-09-20 18:30:06 +1000

    Remove API request code

--- src/actions/todo.js
+++ src/actions/todo.js
@@ -1,19 +1,9 @@
-const API_ENDPOINT = (location.hostname === 'localhost') ? "http://localhost:3000" : "https://todo-backend-seven-nu.vercel.app";
+const API_ENDPOINT = "http://localhost:3000";
 
 export async  function  getAllToDos() {
-    const response = await fetch(`${API_ENDPOINT}/v1/todos?limit=10`);
-    const data = await response.json();
-    return data;
+    
 }
 
 export async  function  addToDo(todo) {
-    const response = await fetch(`${API_ENDPOINT}/v1/todos`, {
-        method: 'POST',
-        body: JSON.stringify(todo),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    const data = await response.json();
-    return data;
+    
 }
